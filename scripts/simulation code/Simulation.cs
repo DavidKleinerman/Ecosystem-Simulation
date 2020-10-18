f29f01ad018759617c5923b36fa70e3963290540
DavidKleinerman <72667884+DavidKleinerman@users.noreply.github.com>
2020-10-18 09:19:11 +0300

    Finished tile despawn

--- scripts/simulation code/Simulation.cs
+++ scripts/simulation code/Simulation.cs
@@ -9,6 +9,9 @@ public class Simulation : Spatial
 	private PackedScene GrasslandTile = (PackedScene)GD.Load("res://assets/biomes/GrasslandTile.tscn");
 	private PackedScene TundraTile = (PackedScene)GD.Load("res://assets/biomes/TundraTile.tscn");
 
+	private int selectedBiome = -1; //forest = 0, grassland = 1, desert = 2, tundra = 3, water = 5, none = -1
+	private bool isWorldBuilding = true;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -27,9 +30,59 @@ public class Simulation : Spatial
 		}
 	}
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(float delta)
+	{
+		
+
+	}
+
+	public override void _PhysicsProcess(float delta)
+	{
+		if (isWorldBuilding && Input.IsActionPressed("ui_select"))
+			ReplaceBiome();
+	}
+
+	private void _on_ItemList_item_selected(int index)
+	{
+		selectedBiome = index;
+		GD.Print(index, "\n");
+	}
+
+	private void ReplaceBiome(){
+		Node selectedTile = GetObjectUnderMouse();
+		GD.Print(selectedTile.Name);
+		if (selectedTile.Name == "StaticBody"){
+			Vector3 selectedPos = ((Spatial)selectedTile).ToGlobal(((Spatial)selectedTile).Translation);
+			GD.Print(selectedPos);
+			foreach (Node n in GetChildren()){
+				if (n.Name != "Control" && n.Name != "SpeciesHolder"){
+					Vector3 position = ((Spatial)n).Translation;
+					if (position.x == selectedPos.x && position.y == selectedPos.y && position.z == selectedPos.z){
+						GD.Print("Found it!\n");
+						n.QueueFree();
+						break;
+					}
+				}
+			}
+			//if (selectedBiome == 0)
+		}
+
+	}
+
+	private Node GetObjectUnderMouse(){
+		Vector2 mousePos = (Vector2) new Vector2(0, 0);
+		mousePos = GetViewport().GetMousePosition();
+		Vector3 rayFrom = ToGlobal(((Camera)GetNode("CameraHolder/Camera")).ProjectRayOrigin(mousePos));
+		Vector3 rayTo = rayFrom + ToGlobal(((Camera)GetNode("CameraHolder/Camera")).ProjectRayNormal(mousePos)) * 20000;
+		PhysicsDirectSpaceState spaceState = GetWorld().DirectSpaceState;
+		var hit = spaceState.IntersectRay(rayFrom, rayTo);
+		Node selection = (Node) new Node();
+		if (hit.Contains("collider"))
+			selection = (Node)hit["collider"];
+		return selection;
+	}
 }
+
+
+
