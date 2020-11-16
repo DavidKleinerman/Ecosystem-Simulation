f560d411d8565f3c9fbb4fda5c3407b3703437b1
DavidKleinerman <72667884+DavidKleinerman@users.noreply.github.com>
2020-11-16 18:22:19 +0200

    improved input checking in "add new species" menu

--- scripts/sim UI code/new species menu/Trait.cs
+++ scripts/sim UI code/new species menu/Trait.cs
@@ -6,14 +6,30 @@ public class Trait : Control
 
 
 	private String validFloat;
+
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(float delta)
 	{
 		if(Input.IsActionJustPressed("ui_accept")){
-			if(!GetNode<SpinBox>("SpinBox").GetLineEdit().Text.IsValidFloat())
+			CorrectValue();
+		}
+	}
+
+	public override void _Input(InputEvent inputEvent)
+	{
+		if (inputEvent is InputEventMouseButton mouseButton){
+
+			if(!(new Rect2(GetNode<SpinBox>("SpinBox").GetLineEdit().RectGlobalPosition, GetNode<SpinBox>("SpinBox").GetLineEdit().RectSize).HasPoint(((InputEventMouseButton)inputEvent).Position))){
+				CorrectValue();
+			}
+		}
+	}
+
+	private void CorrectValue(){
+		if(!GetNode<SpinBox>("SpinBox").GetLineEdit().Text.IsValidFloat())
 				GetNode<SpinBox>("SpinBox").GetLineEdit().Text = (GetNode<HSlider>("HSlider").Value.ToString());
 				GetNode<SpinBox>("SpinBox").Value = GetNode<HSlider>("HSlider").Value;
-		}
+				GetNode<SpinBox>("SpinBox").GetLineEdit().ReleaseFocus();
 	}
 
 	private void _on_SpinBox_value_changed(float value)
@@ -30,7 +46,4 @@ public class Trait : Control
 
 
 }
-
-
-
 
