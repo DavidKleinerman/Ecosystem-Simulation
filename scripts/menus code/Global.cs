017a263e06b79ca4ec751722139affd5826136da
Eli Mor <69359863+EliMor1@users.noreply.github.com>
2021-03-21 20:39:15 +0800

    Launch Simulation Menu
    Added functionality to launch sim menu screen
    
    Launch Simulation Menu Screen is now fully completed.

--- scripts/menus code/Global.cs
+++ scripts/menus code/Global.cs
@@ -8,4 +8,7 @@ public class Global : Node
 	public static Vector2 Resolution;
 	public static bool enableShadows = false;
 	//public static int A = 0;
+	public static int worldSize = 32;
+	public static float biomeGrowthRate = 3;
+	public static int biomeType = 0;
 }
--- scripts/menus code/LaunchSim.cs
+++ scripts/menus code/LaunchSim.cs
@@ -3,13 +3,65 @@ using System;
 
 public class LaunchSim : Button
 {
+	public enum BiomeTypeSet {
+	 	Water = 0,
+	 	Forest = 1,
+	 	Desert = 2,
+	 	Grass = 3,
+	 	Tundra = 4
+	}
+	public enum WorldSizeSet {
+		Small = 16,
+		Normal = 24,
+		Big = 32
+	};
 	
-
+	private void _on_WorldSizePicker_item_selected(int index)
+	{
+		GD.Print(index, "\n");
+		if(index == 0){
+			Global.worldSize = (int)WorldSizeSet.Small;
+		}
+		else if(index == 1){
+			Global.worldSize = (int)WorldSizeSet.Normal;
+		}
+		else if(index == 2){
+			Global.worldSize = (int)WorldSizeSet.Big;
+			
+		}
+	}
+	private void _on_BiomeType_item_selected(int index)
+	{
+		GD.Print(index, "\n");
+		if(index == 0){
+			Global.biomeType= (int)BiomeTypeSet.Water;
+		}
+		if(index == 1){
+			Global.biomeType= (int)BiomeTypeSet.Forest;
+		}
+		if(index == 2){
+			Global.biomeType= (int)BiomeTypeSet.Desert;
+		}
+		if(index == 3){
+			Global.biomeType= (int)BiomeTypeSet.Grass;
+		}
+		if(index == 4){
+			Global.biomeType= (int)BiomeTypeSet.Tundra;
+		}
+	}
+	private void _on_HSlider_value_changed(float value)
+	{
+		Global.biomeGrowthRate = value;
+		GD.Print(Global.biomeGrowthRate);
+	}
 	private void _on_LaunchSim_pressed()
 	{
 		GetTree().ChangeScene("res://assets/Simulation.tscn");
 	}
 }
+
+
+
 
 
 
