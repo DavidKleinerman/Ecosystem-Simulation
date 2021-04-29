71893f5c8db478601b8bbf04f824b2781305776a
DavidKleinerman <72667884+DavidKleinerman@users.noreply.github.com>
2021-04-29 14:25:18 +0300

    Simulation rate selector visibility is now based on context

--- scripts/sim UI code/SimulationRate.cs
+++ scripts/sim UI code/SimulationRate.cs
@@ -12,11 +12,48 @@ public class SimulationRate : ItemList
 
 	private void _on_StartSimulation_pressed()
 	{
+		MakeVisible();
+	}
+
+	void MakeVisible(){
 		Visible = true;
 		GetParent().GetNode<Label>("Label").Visible = true;
 	}
 
+	void MakeInvisible(){
+		Visible = false;
+		GetParent().GetNode<Label>("Label").Visible = false;
+	}
+
+	private void _on_AddNewSpecies_pressed()
+	{
+		MakeInvisible();
+	}
+
+	private void _on_DisplayCharts_pressed()
+	{
+		MakeInvisible();
+	}
+
+
+	private void _on_Pause_pressed()
+	{
+		MakeInvisible();
+	}
+	
+	private void _on_CloseNewSpecies_pressed()
+	{
+		MakeVisible();
+	}
+	
+	private void _on_CloseChartMenu_pressed()
+	{
+		MakeVisible();
+	}
+	
+	private void _on_Resume_pressed()
+	{
+		MakeVisible();
+	}
+
 }
-
-
-
