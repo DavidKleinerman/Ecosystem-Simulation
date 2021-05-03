db6222635527bf0e50975ab543587d2e79295680
Eli Mor <69359863+EliMor1@users.noreply.github.com>
2021-05-04 03:08:54 +0800

    Fixed a bug
    Now New Simulation settings menu is staying set when coming back to main menu from a running simulation.

--- scripts/menus code/NewSimMenu.cs
+++ scripts/menus code/NewSimMenu.cs
@@ -8,11 +8,23 @@ public class NewSimMenu : Control
 	// private string b = "text";
 
 	// Called when the node enters the scene tree for the first time.
+	private int WorldSize = Global.worldSize;
+	private int BiomeType = Global.biomeType;
+	private float BiomeGrowthRate = Global.biomeGrowthRate;
 	public override void _Ready()
 	{
 		this.Visible = true;
-		GetNode<Godot.ItemList>("WorldSizePicker").Select(2);
-		GetNode<Godot.ItemList>("BiomeType").Select(0);
+		if(this.WorldSize == 32){
+			GetNode<Godot.ItemList>("WorldSizePicker").Select(2);
+		}
+		else if(this.WorldSize == 24){
+			GetNode<Godot.ItemList>("WorldSizePicker").Select(1);
+		}
+		else if(this.WorldSize == 16){
+			GetNode<Godot.ItemList>("WorldSizePicker").Select(0);
+		}
+		GetNode<Godot.ItemList>("BiomeType").Select(BiomeType);
+		//GetNode<VSlider>("VSlider").Select(BiomeGrowthRate);
 		GetViewport().Size = Global.Resolution;
 	}
 
