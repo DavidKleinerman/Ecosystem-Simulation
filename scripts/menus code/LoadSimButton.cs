d01f0233ac6dce646bb42739b6134e091f0be27d
DavidKleinerman <72667884+DavidKleinerman@users.noreply.github.com>
2021-05-03 11:28:44 +0300

    added try catch to load sim save file

--- scripts/menus code/LoadSimButton.cs
+++ scripts/menus code/LoadSimButton.cs
@@ -10,17 +10,22 @@ public class LoadSimButton : Button
 
 	private void _on_FileDialog_confirmed()
 	{
-		Global.IsLoaded = true;
-		String path = GetParent().GetNode<FileDialog>("FileDialog").CurrentPath;
-		var saveFile = new File();
-		if (!saveFile.FileExists(path)){
-			GD.Print("problem in opening file in path: " + path);
+		try{
+			Global.IsLoaded = true;
+			String path = GetParent().GetNode<FileDialog>("FileDialog").CurrentPath;
+			var saveFile = new File();
+			if (!saveFile.FileExists(path)){
+				GD.Print("problem in opening file in path: " + path);
+				return;
+			}
+			saveFile.Open(path, File.ModeFlags.Read);
+			var loadedData = new Godot.Collections.Array((Godot.Collections.Array)JSON.Parse(saveFile.GetLine()).Result);
+			Global.LoadedArray = loadedData;
+			GetTree().ChangeScene("res://assets/Simulation.tscn");
+		} catch (Exception e) {
 			return;
 		}
-		saveFile.Open(path, File.ModeFlags.Read);
-		var loadedData = new Godot.Collections.Array((Godot.Collections.Array)JSON.Parse(saveFile.GetLine()).Result);
-		Global.LoadedArray = loadedData;
-		GetTree().ChangeScene("res://assets/Simulation.tscn");
+		
 	}
 
 }
